90c590d62ba00fb9c7997ea08ada1bfee3651814
Anais <akayaturan@gmail.com>
2018-04-13 12:10:41 +0200

    AE lstm

---
+++ anais/ae_lstm.py
@@ -0,0 +1,23 @@
+from keras.layers import Input, LSTM, RepeatVector
+from keras.models import Model
+import numpy as np
+from keras.callbacks import TensorBoard
+
+data = np.load('../data/data_matrix.npz')['arr_0']
+
+latent_dim = 50
+timesteps = 1221
+input_dim = 242
+
+inputs = Input(shape=(timesteps, input_dim))
+encoded = LSTM(latent_dim)(inputs)
+
+decoded = RepeatVector(timesteps)(encoded)
+decoded = LSTM(input_dim, return_sequences=True)(decoded)
+
+sequence_autoencoder = Model(inputs, decoded)
+encoder = Model(inputs, encoded)
+
+sequence_autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+sequence_autoencoder.fit(data, data, epochs=50, batch_size=16, shuffle=True, validation_data=(data, data),
+                         callbacks=[TensorBoard(log_dir='/tb_logs', histogram_freq=0, write_graph=False)])
