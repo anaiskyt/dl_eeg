5f021a5cd9895b061862ff66a7a82e5640217c33
Anais <akayaturan@gmail.com>
2018-04-14 20:42:50 +0200

    ae lstm

--- anais/ae_lstm.py
+++ anais/ae_lstm.py
@@ -1,12 +1,13 @@
 from keras.layers import Input, LSTM, RepeatVector
 from keras.models import Model
 import numpy as np
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, EarlyStopping
 from keras.optimizers import SGD, RMSprop
+
 
 data = np.load('../data/data_matrix.npz')['arr_0']
 
-latent_dim = 50
+latent_dim = 128
 timesteps = 1221
 input_dim = 242
 
@@ -20,10 +21,10 @@ sequence_autoencoder = Model(inputs, decoded)
 encoder = Model(inputs, encoded)
 
 sgd = SGD(lr=5, momentum=1.5, decay=1e-6, nesterov=False)
-rmsprop = RMSprop(lr=1, rho=0.9)
+rmsprop = RMSprop(lr=0.1, rho=0.9)
 
 sequence_autoencoder.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
-sequence_autoencoder.fit(data, data, epochs=50, batch_size=256, shuffle=True, validation_data=(data, data),
-                         callbacks=[TensorBoard(log_dir='../logs', histogram_freq=0, write_graph=False)])
+sequence_autoencoder.fit(data, data, epochs=10, batch_size=128, shuffle=True, validation_data=(data, data),
+                         callbacks=[TensorBoard(log_dir='./logs/ae_128_batch_128_epoch_10_lr01', histogram_freq=0, write_graph=False), EarlyStopping(patience=3, min_delta=0)])
 encoder.save_weights('weights_encoder.h5')
 encoder.save('encoder.h5')
