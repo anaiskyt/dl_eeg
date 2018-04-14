80786af1aa2e8384c76266ba1985c4aceb6336d8
Ubuntu <anais@deepproject.mc4nig5smeeerccfneoytp1f5d.fx.internal.cloudapp.net>
2018-04-14 22:17:39 +0000

    New logs

--- anais/autoencoder.py
+++ anais/autoencoder.py
@@ -19,26 +19,25 @@ input_dim = 242
 inputs = Input(shape=(timesteps, input_dim))
 
 encoder = Sequential()
+encoder.add(LSTM(latent_dim, input_shape=(timesteps, input_dim)))
 encoder.load_weights('./weights_encoder.h5', by_name=True)
 encoder.trainable = False
+encoder = encoder(inputs)
 
-encoded = Model(inputs, encoder)
-
-layer_1 = Dense(64, input_shape=(128, 128), activation='relu')(encoded)
+layer_1 = Dense(64, input_shape=(128, 128), activation='relu')(encoder)
 layer_1 = Dropout(0.5)(layer_1)
 layer_2 = Dense(64, activation='relu')(layer_1)
 layer_2 = Dropout(0.5)(layer_2)
 layer_3 = Dense(5, activation='softmax')(layer_2)
 
-complete_model = Model(encoded, layer_3)
-
+complete_model = Model(inputs, layer_3)
 
 complete_model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
 
-batch_size = [128, 256]
+batch_size = [12]
 epochs = [10, 50, 100]
 
 for batch in batch_size:
@@ -49,9 +48,9 @@ for batch in batch_size:
         tensorboard = TensorBoard(log_dir=directory.format(time()), write_graph=True, write_images=True)
 
         complete_model.fit(x_train, y_train,
-                           epochs=10,
-                           batch_size=128,
+                           epochs=epoch,
+                           batch_size=batch,
                            callbacks=[tensorboard, EarlyStopping(patience=3, min_delta=0)])
-        score = complete_model.evaluate(x_test, y_test, batch_size=128)
+        score = complete_model.evaluate(x_test, y_test, batch_size=batch)
 
         complete_model.save('complete_model.h5')
